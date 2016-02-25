451466b332bec51a30b30ae1fc1f42adbee3d966
WslF <WasylF@gmail.com>
2016-02-26 01:32:21 +0200

    add call to Partition

--- PS-System/src/wslf/Main.java
+++ PS-System/src/wslf/Main.java
@@ -1,5 +1,8 @@
 package wslf;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  *
  * @author Wsl_F
@@ -11,6 +14,14 @@ public class Main {
      */
     public static void main(String[] args) {
         
+        List<int[]> list= new LinkedList<>();
+        Partition.generate(10, 5, list);
+        list.stream().forEach((partition) -> {
+            System.out.print("# ");
+            for (int i= 0; i<partition.length; i++)
+                System.out.print(partition[i]+" ");
+            System.out.println("");
+        });
     }
     
 }
